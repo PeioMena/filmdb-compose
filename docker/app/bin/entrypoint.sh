99f545f1313d5c19e9c4d170ee07b21de37e2f52
Peio <pmena@codesyntax.com>
2023-03-14 16:44:30 +0100

    filmdb-compose 2.0

--- docker/app/bin/entrypoint.sh
+++ docker/app/bin/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Run migrations and collect static files
-#
+python bin/manage.py collectstatic --noinput
+python bin/manage.py migrate --noinput
 
 # Run command specified when running the container
 echo "Run: $@"
